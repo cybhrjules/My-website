d5e1e2866c1d4ed8cb485e1d436ba9d54ee51561
cybhrjules <102770886+cybhrjules@users.noreply.github.com>
2024-09-12 00:10:07 +0200

    Add files via upload

---
+++ jules.js
@@ -0,0 +1,16 @@
+document.querySelector("h3").innerHTML = "Academica"
+
+class MakuzaFamily extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>I know it</h1>
+      </div>
+    )
+  }
+}
+
+
